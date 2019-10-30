7a526b5624fb0bb014ea7d08eb7e9a1c42bdae63
unknown <fjmiranda4iuandes.cl>
2019-10-29 22:13:05 -0300

    first

---
+++ WindowsFormsApp1/Controller.cs
@@ -0,0 +1,40 @@
+﻿using ModelosRally;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VistasRally
+{
+    class Controller
+    {
+        List<Persona> Pilotos;
+        Form1 vistaPilotos;
+
+
+        
+        public Controller(Form1 vistaPilotos)
+        // El controlador reciBe una forma 
+        {
+            Pilotos = new List<Persona>(); //Crea un objeto piloto
+            this.vistaPilotos = vistaPilotos; 
+            this.vistaPilotos.OnAgregarPiloto += VistaPilotos_OnAgregarPiloto; // suscribe un evento vista_piloto
+         }
+
+        private void VistaPilotos_OnAgregarPiloto(object sender, AgregarPilotoEventArgs e)
+        {
+            //agregar pilotos
+
+            Persona piloto = new Persona(e.nombre, Rol.Piloto);// creo piloto nuevo con sus caract
+            Pilotos.Add(piloto);//lo agrego a la lista
+            vistaPilotos.ActualizarListadoPilotos(piloto);//ejecuto el metodo de agregar piloto que hice en el forms
+            
+            //este metodo se va a suscribir al controlador de arriba en la forma se creará el evento,
+            //de que cuando no esté suscrito haga eso, y así irá agregando los datos correspondientes.
+        }
+
+
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,63 @@
+﻿using ModelosRally;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VistasRally
+{
+    public partial class Form1 : Form
+    {
+        public event EventHandler<AgregarPilotoEventArgs> OnAgregarPiloto;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.RolComboBox.DataSource = Enum.GetValues(typeof(Rol));
+        }
+
+        private void AgregarPilotoButton_Click(object sender, EventArgs e)//Boton agregar piloto
+        {
+            if (OnAgregarPiloto != null) // si el evento no está suscrito que haga lo sgte
+            {
+                AgregarPilotoEventArgs pilotoArgs = new AgregarPilotoEventArgs(); //crear el objeto
+                pilotoArgs.nombre = this.NameTextBox.Text; //el textbox se va a llamar de esa forma
+                OnAgregarPiloto(this, pilotoArgs);
+                // object sender = lo que escribo le va a llegar al OnAgregarPiloto 
+            }
+               
+        }
+
+        public void ActualizarListadoPilotos(Persona pilotoNuevo)//metodo para agregar pilotos en la forma
+
+        {
+            //agrega un piloto a la lista
+            PilotNameComboBox.Items.Add(pilotoNuevo);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RolComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PilotNameComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NameTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
